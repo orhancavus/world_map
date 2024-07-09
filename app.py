382d37f5e8dc1b9cb4f7b6ab0da0019f6dbdcf25
Orhan Çavuş <orhancv@gmail.com>
2024-07-10 00:01:20 +0300

    map variations

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import cartopy.feature as cfeature
 fig, ax = plt.subplots(figsize=(15, 10), subplot_kw={"projection": ccrs.PlateCarree()})
 
 # Add coastlines and borders
-ax.add_feature(cfeature.COASTLINE, edgecolor="gray", linewidth=0.5)
-ax.add_feature(cfeature.BORDERS, edgecolor="gray", linewidth=0.5)
+ax.add_feature(cfeature.COASTLINE, edgecolor="black", linewidth=0.5)
+ax.add_feature(cfeature.BORDERS, edgecolor="black", linewidth=0.5)
 
 # Add land feature (continents) in white
-ax.add_feature(cfeature.LAND, facecolor="white", edgecolor="gray")
+ax.add_feature(cfeature.LAND, facecolor="white", edgecolor="black")
 
 # Set the background color to black
 ax.set_facecolor("black")
--- app_dots.py
+++ app_dots.py
@@ -1,15 +1,38 @@
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-import matplotlib.patches as mpatches
+import random
 from cartopy.io import shapereader
+
+colors = [
+    "blue",
+    "red",
+    "orange",
+    "green",
+    "gray",
+    "magenta",
+    "brown",
+    "cyan",
+    "purple",
+    "pink",
+    "yellow",
+    "teal",
+    "lavender",
+    "lime",
+    "turquoise",
+    "maroon",
+    "gold",
+    "olive",
+    "navy",
+]
+
 
 # Create a new figure and axis with a specific projection
 fig, ax = plt.subplots(figsize=(15, 10), subplot_kw={"projection": ccrs.PlateCarree()})
 
 # Add coastlines and borders
-ax.add_feature(cfeature.COASTLINE, edgecolor="gray", linewidth=0.5)
-ax.add_feature(cfeature.BORDERS, edgecolor="gray", linewidth=0.5)
+ax.add_feature(cfeature.COASTLINE, edgecolor="black", linewidth=0.5)
+ax.add_feature(cfeature.BORDERS, edgecolor="black", linewidth=0.5)
 
 # Get the Natural Earth shapefile for land
 shapename = "physical"
@@ -18,13 +41,14 @@ reader = shapereader.Reader(land_shp)
 
 # Add land feature with a white dot pattern
 for geometry in reader.geometries():
+    random_color = random.choice(colors)
     ax.add_geometries(
         [geometry],
         ccrs.PlateCarree(),
         facecolor="none",
-        edgecolor="gray",
-        linewidth=0.5,
-        hatch="...",
+        edgecolor="orange",
+        linewidth=0.1,
+        hatch="..",
     )
 
 # Set the background color to black
@@ -39,4 +63,6 @@ plt.title("World Map", color="white", fontsize=20)
 
 # Adjust layout and display the map
 plt.tight_layout()
+fn = random.randint(1, 1000000)
+plt.savefig(f"maps/world_map_{fn}.png", dpi=300)
 plt.show()
